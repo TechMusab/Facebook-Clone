edfcac61b3bb28cf0b77c5cb672e172cebd89ed8
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-01 14:58:51 -0700

    Added Dark Mode

--- script.js
+++ script.js
@@ -7,4 +7,21 @@ const dark=document.querySelector('.dark');
 dark.addEventListener('click',()=>{
     dark.classList.toggle('on');
     document.body.classList.toggle('darktheme');
+    if(localStorage.getItem('theme')=='dark'){
+        localStorage.setItem('theme','light');
+    }
+    else{
+        localStorage.setItem('theme','dark');
+    }
 });
+if(localStorage.getItem('theme')=='dark'){
+    dark.classList.add('on');
+    document.body.classList.add('darktheme');
+}
+else if(localStorage.getItem('theme')=='light'){
+    dark.classList.remove('on');
+    document.body.classList.remove('darktheme');
+}
+else{
+    localStorage.setItem('theme','dark')
+}
